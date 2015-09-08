3747e076f5113db6ce434fcdee2b6843aee02d7d
Jeff Lee <jeff.lee.java@gmail.com>
2015-09-08 16:31:45 +1000

    First commit

---
+++ LayoutConstraint/LayoutConstraint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutConstraint
+//
+//  Created by Jeff Lee on 8/09/2015.
+//  Copyright (c) 2015 Jeff Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
