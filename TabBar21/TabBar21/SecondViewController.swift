203545aede92850f53db290ba2ac561d65fb1be4
Jeff Lee <jeff.lee.java@gmail.com>
2015-09-08 23:19:16 +1000

    .

---
+++ TabBar21/TabBar21/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBar21
+//
+//  Created by Jeff Lee on 8/09/2015.
+//  Copyright (c) 2015 Jeff Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
